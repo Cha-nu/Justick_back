d27d2e4c55b4f22170ebb1e7a7f177f9b28b5c14
Cha-nu <cksdn43@cau.ac.kr>
2025-03-11 19:49:28 +0900

    [upload] dev framework

---
+++ Justick/src/main/java/com/project/Justick/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.project.Justick.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/test")
+    public String hello() {
+        return "테스트입니다.";
+    }
+}
