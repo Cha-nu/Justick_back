85c3fab63dfd772247d17b09821297d3ffcb4961
Cha-nu <cksdn43@cau.ac.kr>
2025-03-11 19:09:16 +0900

    init

---
+++ Justick/src/main/java/com/project/Justick/JustickApplication.java
@@ -0,0 +1,13 @@
+package com.project.Justick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustickApplication.class, args);
+	}
+
+}
---
+++ Justick/src/test/java/com/project/Justick/JustickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Justick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
