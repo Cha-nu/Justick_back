b4d0a758c6cc1f1d6767399e0fa9fdb814cd02af
Cha-nu <cksdn43@cau.ac.kr>
2025-05-28 17:51:38 +0900

    [fix] 오타 수정

--- Justick/src/main/java/com/project/Justick/DTO/Tomato/TomatoRequest.java
+++ Justick/src/main/java/com/project/Justick/DTO/Tomato/TomatoRequest.java
@@ -6,5 +6,6 @@ import lombok.Setter;
 
 @Getter @Setter
 public class TomatoRequest extends AgriculturalRequest {
+    private int intake;
     private String grade; // 등급 SPECIAL, HIGHTomatoRequest
 }
